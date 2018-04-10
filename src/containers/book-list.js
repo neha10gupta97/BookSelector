c05906e4e278acafc9438f93121529cf1ad8d572
Neha Gupta <neha10gupta97@gmail.com>
2018-04-10 14:37:09 +0530

    book-list component added

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList from Component{
-
+class BookList extends Component{
   renderList(){
     return this.props.books.map((book) => {
         return(
@@ -18,3 +18,11 @@ export default class BookList from Component{
     )
   }
 }
+
+function mapStateToProps(state){
+  return{
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
